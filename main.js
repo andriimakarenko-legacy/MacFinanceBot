5ab8ea9062dcbd8eab91209023f684626c22c12e
Andrii Makarenko <andriimakarenko@outlook.com>
2019-09-08 20:16:30 +0300

    Works but separation of concerns is broken for now

--- main.js
+++ main.js
@@ -12,14 +12,26 @@
 
 const readlineSync = require('readline-sync');
 let MonoUser = require('./mono.js');
-let TGUser = require('./tg.js');
+let TelegramBot = require('node-telegram-bot-api');
 
 const monoToken = readlineSync.question('Please provide your Monobank API token: ');
 const telegramToken = readlineSync.question('Please provide your Telegram API token: ');
-const chatID = readlineSync.question('Please specify the Telegram chat ID where to send updates: ');
 const maxOKDiff = readlineSync.question('How much UAH do you allow to spend?: ');
 const startingBalance = 1196000;
 let currentBalance = 1196000;
+
+const authorizedIDs = [369190174, 202478614];
+const updateReceiverChats = [];
+const telegramBot = new TelegramBot(telegramToken, { polling: true });
+
+telegramBot.on('message', msg => {
+	if (authorizedIDs.includes(msg.from.id)) {
+		let chatID = msg.chat.id;
+		updateReceiverChats.push(chatID);
+		telegramBot.sendMessage(chatID, 'You are now registered and will receive updates upon every transaction');
+		console.log(updateReceiverChats);
+	}
+});
 
 setInterval(() => {
 	var request = require('request');
@@ -28,12 +40,13 @@ setInterval(() => {
 	.then(balance => {
 		if (balance == currentBalance) return;
 		let difference = (startingBalance - balance)/100;
-
 		let message = 'You\'ve made a new purchase. ';
 		message += 'You have ';
 		message += maxOKDiff - difference;
 		message += ' UAH left to spend';
 		currentBalance = balance;
-		console.log(message);
+		// console.log(message);
+
+		updateReceiverChats.forEach(chatID => {telegramBot.sendMessage(chatID, message)});
 	});
 }, 70000);
